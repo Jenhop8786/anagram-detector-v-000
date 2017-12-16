7a7f29a4e5cf22fb68d42ca5a0425a2e11f5e3c6
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-16 16:05:58 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,19 +1,19 @@
 # Your code goes here!
-class Anagram 
-  attr_accessor :word 
+class Anagram
+  attr_accessor :word
 
   def initialize(word)
-    @word = word 
-  end 
+    @word = word
+  end
 
   def match(collection)
     output = []
     collection.each_with_index do |el, idx|
       if el.split("").sort.join == word.split("").sort.join
         output << collection[idx]
-      end 
-    end 
-    output 
-  end 
+      end
+    end
+    output
+  end
 
-end 
+end
